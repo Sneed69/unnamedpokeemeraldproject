fead696e9cc8d375a1968b03cb50a76a6f4dd16c
questbugreporter <devilduke1995@hotmail.com>
2022-04-06 18:21:03 +0300

    minor fix

--- include/constants/item.h
+++ include/constants/item.h
@@ -4,18 +4,16 @@
 // These constants are used in gItems
 #define POCKET_NONE        0
 #define POCKET_ITEMS       1
-#define POCKET_CONSUMABLES 2
-#define POCKET_POKE_BALLS  3
-#define POCKET_TM_HM       4
-#define POCKET_BERRIES     5
-#define POCKET_KEY_ITEMS   6
+#define POCKET_POKE_BALLS  2
+#define POCKET_TM_HM       3
+#define POCKET_BERRIES     4
+#define POCKET_KEY_ITEMS   5
 
 #define ITEMS_POCKET       0
-#define CONSUMABLES_POCKET 1
-#define BALLS_POCKET       2
-#define TMHM_POCKET        3
-#define BERRIES_POCKET     4
-#define KEYITEMS_POCKET    5
-#define POCKETS_COUNT      6
+#define BALLS_POCKET       1
+#define TMHM_POCKET        2
+#define BERRIES_POCKET     3
+#define KEYITEMS_POCKET    4
+#define POCKETS_COUNT      5
 
 #endif // GUARD_ITEM_CONSTANTS_H
