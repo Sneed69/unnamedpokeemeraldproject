89fd060ebe24ac053783698d5f56eb24e5e433b0
questbugreporter <devilduke1995@hotmail.com>
2022-04-07 21:07:02 +0300

    pp maxed and pokedex reorder

--- src/birch_pc.c
+++ src/birch_pc.c
@@ -33,37 +33,37 @@ const u8 *GetPokedexRatingText(u16 count)
         return gBirchDexRatingText_LessThan40;
     if (count < 50)
         return gBirchDexRatingText_LessThan50;
-    if (count < 60)
+    if (count < 75)
         return gBirchDexRatingText_LessThan60;
-    if (count < 70)
-        return gBirchDexRatingText_LessThan70;
-    if (count < 80)
-        return gBirchDexRatingText_LessThan80;
-    if (count < 90)
-        return gBirchDexRatingText_LessThan90;
-    if (count < 100)
-        return gBirchDexRatingText_LessThan100;
-    if (count < 110)
-        return gBirchDexRatingText_LessThan110;
     if (count < 120)
-        return gBirchDexRatingText_LessThan120;
+        return gBirchDexRatingText_LessThan70;
     if (count < 140)
-        return gBirchDexRatingText_LessThan130;
+        return gBirchDexRatingText_LessThan80;
     if (count < 160)
-        return gBirchDexRatingText_LessThan140;
+        return gBirchDexRatingText_LessThan90;
     if (count < 180)
-        return gBirchDexRatingText_LessThan160;
-    if (count < 210)
-        return gBirchDexRatingText_LessThan150;
+        return gBirchDexRatingText_LessThan100;
+    if (count < 200)
+        return gBirchDexRatingText_LessThan110;
+    if (count < 230)
+        return gBirchDexRatingText_LessThan120;
     if (count < 240)
+        return gBirchDexRatingText_LessThan130;
+    if (count < 250)
+        return gBirchDexRatingText_LessThan140;
+    if (count < 300)
+        return gBirchDexRatingText_LessThan160;
+    if (count < 325)
+        return gBirchDexRatingText_LessThan150;
+    if (count < MY_DEX_COUNT - 65)
         return gBirchDexRatingText_LessThan170;
-    if (count < 275)
+    if (count < MY_DEX_COUNT - 40)
         return gBirchDexRatingText_LessThan180;
-    if (count < 400)
+    if (count < MY_DEX_COUNT - 15)
         return gBirchDexRatingText_LessThan190;
-    if (count < NATIONAL_DEX_COUNT)
+    if (count < MY_DEX_COUNT)
         return gBirchDexRatingText_LessThan200;
-    if (count == NATIONAL_DEX_COUNT)
+    if (count == MY_DEX_COUNT)
         return gBirchDexRatingText_DexCompleted;
     return gBirchDexRatingText_LessThan10;
 }
