ec937efa8bfa972159839d0139f8af96a6e2d911
questbugreporter <devilduke1995@hotmail.com>
2022-04-10 00:03:05 +0300

    nickname fom summary

--- src/strings.c
+++ src/strings.c
@@ -1825,6 +1825,7 @@ const u8 gText_Answer[] = _("Answer");
 const u8 gText_PokeBalls[] = _("Poké Balls");
 const u8 gText_Berry[] = _("Berry");
 const u8 gText_Berries[] = _("Berries");
+const u8 gText_Nickname[] = _("Nickname");
 //HGSS_Ui PokedexPlus
 const u8 gText_Stats_Buttons[]  = _("{A_BUTTON}TOGGLE   {DPAD_UPDOWN}MOVES");
 const u8 gText_Stats_Buttons_Decapped[]  = _("{A_BUTTON}Toggle   {DPAD_UPDOWN}Moves");
