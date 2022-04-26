864989926a55cc37f256d9dd89d4fac8f27ea2fb
Sneed69 <devilduke1995@hotmail.com>
2022-04-26 05:22:44 +0300

    improve fog on objects

--- src/field_weather.c
+++ src/field_weather.c
@@ -667,6 +667,7 @@ static void ApplyFogBlend(u8 blendCoeff, u16 blendColor)
     u16 curPalIndex;
 
     BlendPalette(0, 256, blendCoeff, blendColor);
+	BlendPalettesGradually(0x3FF0000, 0, 3, 8, RGB_WHITEALPHA, 0, 0); //blend objects
     color = *(struct RGBColor *)&blendColor;
     rBlend = color.r;
     gBlend = color.g;
@@ -724,8 +725,8 @@ static bool8 LightenSpritePaletteInFog(u8 paletteIndex)
             return TRUE;
     }
     
-    if (IsObjectEventPaletteIndex(paletteIndex))
-        return TRUE;
+    //if (IsObjectEventPaletteIndex(paletteIndex))
+        //return TRUE;
 
     return FALSE;
 }
