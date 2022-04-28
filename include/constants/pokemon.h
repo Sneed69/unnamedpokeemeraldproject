36cf848883a36d7f8242aed1cc964f18ccfeb043
Sneed69 <devilduke1995@hotmail.com>
2022-04-28 14:38:51 +0300

    remove STAT2 globals

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -200,12 +200,7 @@
 #define MON_DATA_KNOWN_MOVES       81
 #define MON_DATA_RIBBON_COUNT      82
 #define MON_DATA_RIBBONS           83
-#define MON_DATA_ATK2              84
-#define MON_DATA_DEF2              85
-#define MON_DATA_SPEED2            86
-#define MON_DATA_SPATK2            87
-#define MON_DATA_SPDEF2            88
-#define MON_DATA_HIDDEN_NATURE     89
+#define MON_DATA_HIDDEN_NATURE     84
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
