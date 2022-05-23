9ecaaddc1b031613c87dd11420f246148bdfa8ad
sneed <devilduke1995@gmail.com>
2022-05-24 00:48:56 +0300

    constants and chanceNotRare

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -90,6 +90,13 @@
 #define DEFAULT_STAT_STAGE 6
 #define MAX_STAT_STAGE    12
 
+// Held Item Chances
+#define ITEM_CHANCE_COMMON         50
+#define ITEM_CHANCE_RARE            5
+// With Compound Eyes
+#define ITEM_CHANCE_ABILITY_COMMON 60
+#define ITEM_CHANCE_ABILITY_RARE   20
+
 // Shiny odds
 #define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
 
