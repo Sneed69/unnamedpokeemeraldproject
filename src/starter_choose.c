df816ad18e26f68f732e8fbe8c369ad2f7aca575
sneed <devilduke1995@gmail.com>
2024-05-01 22:59:14 +0300

    fix starter choosing font colors

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -148,7 +148,7 @@ static const struct BgTemplate sBgTemplates[3] =
     },
 };
 
-static const u8 sTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_WHITE, TEXT_COLOR_LIGHT_GRAY};
+static const u8 sTextColors[] = {TEXT_COLOR_TRANSPARENT, 2, 1};
 
 static const struct OamData sOam_Hand =
 {
